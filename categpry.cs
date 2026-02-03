f525c200cdae107ba937fef19796175b230a9f74
Grimitch <156242381+Grimitch@users.noreply.github.com>
2026-02-03 22:06:24 +0100

    сфеупщкн

---
+++ categpry.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleECommerce
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        
+        public string Description { get; set; }
+        
+        public List<Product> Products { get; set; } = new List<Product>();
+    }
+}
